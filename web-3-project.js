08453ba3d306316e55dab8028c6dd3eb67dc4aa8
Erfan mohammadi <erfan13781998@gmail.com>
2025-01-10 21:35:37 +0330

    web3 project

---
+++ web-3-project.js
@@ -0,0 +1,17 @@
+const specialswiper = new Swiper('#special-box-swiper', {
+    spaceBetween:10,
+    slidesPerView: "auto", 
+    freeMode: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '#next-box-desktop',
+      prevEl: '#prev-box-desktop',
+    },
+  });
+  const swiper = new Swiper('.swiper', {
+    spaceBetween:10,
+    slidesPerView: "auto", 
+    freeMode: true,
+  });
